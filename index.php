34442342414eb571a2fa660bddc979ca75e40447
Darius104 <comandarius@gmail.com>
2022-01-21 16:11:22 +0100

    Censura paragrafo done

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+    <?php  
+        $parola = $_GET['parola'];
+        $paragrafo = "Sono il paragrafo da censurare"
+    ?>
+    <!-- <h1>ciao <?php echo $parola; ?> </h1> -->
+    <h2><?php echo $paragrafo; ?></h2>
+    <h2>Sono lungo <?php echo strlen($paragrafo); ?></h2>
+
+    <h3>--------------------------</h3>
+
+    <h2> <?php echo $censurato = str_replace($parola, '***', $paragrafo) ?> </h2>
+    <h2> Sono lungo <?php echo strlen($censurato) ?> caratteri </h2>
 </body>
 </html>
